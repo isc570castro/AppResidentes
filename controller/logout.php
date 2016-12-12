602394230d808bf51bbe5a0c72d6720ff7e0d6ab
Alejandro Castro Saucedo <Alejandro Castro Saucedo>
2016-12-12 16:50:24 -0600

    Holi

--- controller/logout.php
+++ controller/logout.php
@@ -4,13 +4,13 @@ $usuario = $_SESSION['login'];
 $seguridad = $_SESSION['seguridad'];
 if (!isset($seguridad)) {
 	echo "<SCRIPT TYPE='text/javascript'> alert('Sin acceso'); </SCRIPT>";
-	header('Location: index.php');
+	header ('Location: index.php');
 }
 echo 	"<script type='text/javascript'>
 		alert('Hasta luego ".$usuario."');
 		</script>";
 session_destroy();
 echo 	"<script type='text/javascript'>
-		window.location='../index.html'
+		window.location='../index.php'
 		</script>";		
 ?>
